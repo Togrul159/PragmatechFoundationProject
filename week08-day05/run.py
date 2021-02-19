703c630d4e87cb2d25989bf73e6778881efd4e9c
Togrul159 <69874544+Togrul159@users.noreply.github.com>
2021-02-19 19:38:51 +0400

    Add files via upload

---
+++ week08-day05/run.py
@@ -0,0 +1,19 @@
+#Birinci sual
+def hesab():
+    a=10
+    b=6
+    c=(a+b) 
+    x=(c/2)
+    print(c) 
+    print(x)
+hesab()
+
+# Ikinci Sual
+name= input("name:")
+pasword= input("pasword:")
+if name == "admin" and pasword == "123456":
+    print("Duzdur")
+else:
+    print("Sehvdir")
+
+
