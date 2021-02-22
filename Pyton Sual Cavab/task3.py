3fd721dc9244f1b857f2389a759a07f62b03717b
Togrul159 <69874544+Togrul159@users.noreply.github.com>
2021-02-22 17:29:38 +0400

    Add files via upload

---
+++ Pyton Sual Cavab/task3.py
@@ -0,0 +1,6 @@
+myList=[1,34,56,100,-12,87,987,1,3,5,56,67]
+def cutOlanlar():
+    for i in myList:
+        if i%2==0:
+            print(i)
+cutOlanlar()
---
+++ Pyton Sual Cavab/task4.py
@@ -0,0 +1,10 @@
+myList=[1,34,56,100,-12,87,987,1,3,5,56,67]
+def sayiAzalan():
+    myList.sort()
+    print(myList)
+sayiAzalan()
+
+def sayiCoxalan():
+    myList.sort(reverse=True)
+    print(myList)
+sayiCoxalan()
